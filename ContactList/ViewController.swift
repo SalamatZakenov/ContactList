10fdf9e22b954a8c4d43d63abffa645b29a046bb
SalamatZakenov <szakenov@gmail.com>
2026-07-19 17:01:54 +0500

    Initial Commit

---
+++ ContactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Salamat Zakenov on 19.07.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
